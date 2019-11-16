dde7b1b924ab716345908c3e5c9434b586078a60
Ryan Quitzow-James <ryan.quitzow-james@ligo.org>
2019-11-15 17:24:02 -0800

    Fixing gated data bug by removing code in simulation data output files which was overwriting the original data in the last set of simulated data run (it was being overwritten by the real data run after the simulated data). Adding new requirements files with correct numpy version and separated so that weave will be processed by wheel correctly.

--- simulation_data_output_L1.py
+++ simulation_data_output_L1.py
@@ -65,26 +65,14 @@ ifo = "L1"
 #ifo = "H1"
 
 for i in range(n_injections):
-    print("i")
-    print(i)
-
 
     m1 = mass1
     m2 = mass2
-#    m1 = np.round(np.random.uniform(mass1[0],mass1[1]),2)
-#    m2 = np.round(np.random.uniform(mass2[0],mass2[1]),2)
-    #snr = np.round(np.random.uniform(snr_range[0],snr_range[1]),2)
     snr = snr_json["snrs"][i]
     right_ascension = snr_json["RAs"][i]
     declination = snr_json["declinations"][i]
     polarization = snr_json["polarizations"][i]
 
-    #for ifo in ["L1", "H1"]:
-    #for ifo in ["H1"]:
-    #print(ifo)
-    
-    #testseg, inj_arr = mk_inj.inject_signal(m1, m2, snr, 'L1')
-    #testseg, inj_arr = mk_inj.inject_signal(m1, m2, snr, ifo)
     testseg, inj_arr = mk_inj.inject_signal(m1, m2, snr, ifo, right_ascension, declination, polarization)
     
     inj_param_array[i] = [i, m1, m2] + inj_arr
@@ -111,13 +99,6 @@ for i in range(n_injections):
     
     chisq_array[i] = [i, chisq_min_orig, chisq_min_cut, chisq_min_pred]
 
-    #if i % 10 == 0:
-    #    print("{} out of {} injection frames NNETFIXED successfully!".format(i,n_injections))
-    #    #np.savetxt(os.path.abspath("injections/{0}/{1}/reconstruct_{2}.txt".format(params.label, data_dir, i/10)),PredictData)
-    #    np.savetxt(os.path.abspath(os.path.join(data_dir, "reconstruct_{0}.txt".format(i/10))),PredictData)
-    #    np.savetxt(os.path.abspath(os.path.join(data_dir, "gated_{0}.txt".format(i/10))),CutData)
-    #    #np.savetxt(os.path.abspath("injections/{0}/{1}/original_{2}.txt".format(params.label, data_dir, i/10)),OriginalData)
-    #    np.savetxt(os.path.abspath(os.path.join(data_dir, "original_{0}.txt".format(i/10))),OriginalData)
     np.savetxt(os.path.abspath(os.path.join(data_dir, "reconstruct_{0}_{1}.txt".format(i, ifo))),PredictData)
     np.savetxt(os.path.abspath(os.path.join(data_dir, "gated_{0}_{1}.txt".format(i, ifo))),CutData)
     np.savetxt(os.path.abspath(os.path.join(data_dir, "original_{0}_{1}.txt".format(i, ifo))),OriginalData)
@@ -125,9 +106,6 @@ for i in range(n_injections):
 np.savetxt(os.path.abspath(os.path.join(data_dir, "Inj_snr_" + ifo + ".csv")),SNR_array, fmt='%1.3f',delimiter=',',header="Index, Original, Gated, Reconstructed")
 np.savetxt(os.path.abspath(os.path.join(data_dir, "Inj_chisq_" + ifo + ".csv")),chisq_array,fmt='%1.3f',delimiter=',',header="Index, Original, Gated, Reconstructed")
 np.savetxt(os.path.abspath(os.path.join(data_dir, "Inj_params_" + ifo + ".csv")),inj_param_array,fmt='%1.3f',delimiter=',',header="Index, mass1, mass2, RA, DEC, Polarization, SNR")
-#np.savetxt(os.path.abspath("injections/{0}/{1}/Inj_snr.csv".format(params.label,data_dir)),SNR_array, fmt='%1.3f',delimiter=',',header="Index, Original, Gated, Reconstructed")
-#np.savetxt(os.path.abspath("injections/{0}/{1}/Inj_chisq.csv".format(params.label,data_dir)),chisq_array,fmt='%1.3f',delimiter=',',header="Index, Original, Gated, Reconstructed")
-#np.savetxt(os.path.abspath("injections/{0}/{1}/Inj_params.csv".format(params.label,data_dir)),inj_param_array,fmt='%1.3f',delimiter=',',header="Index, mass1, mass2, RA, DEC, Polarization, SNR")
 
 ################################################
 
@@ -140,8 +118,6 @@ recon_err = abs(orig-recon)/orig * 100
 
 snr_mean = [np.mean(recon_err),np.mean(cut_err)]
 snr_stdev = [np.std(recon_err),np.std(cut_err)]
-#np.savetxt(os.path.abspath("injections/{0}/{1}/snr_mean.txt".format(params.label, data_dir)),snr_mean)
-#np.savetxt(os.path.abspath("injections/{0}/{1}/snr_stdev.txt".format(params.label, data_dir)),snr_stdev)
 np.savetxt(os.path.abspath(os.path.join(data_dir, "snr_mean_" + ifo + ".txt")),snr_mean)
 np.savetxt(os.path.abspath(os.path.join(data_dir, "snr_stdev_" + ifo + ".txt")),snr_stdev)
 
@@ -157,25 +133,16 @@ ch_recon_err = abs(ch_orig-ch_recon)/ch_orig * 100
 chisq_mean = [np.mean(ch_recon_err),np.mean(ch_cut_err)]
 chisq_stdev = [np.std(ch_recon_err),np.std(ch_cut_err)]
 
-#np.savetxt(os.path.abspath("injections/{0}/{1}/chisq_mean.txt".format(params.label, data_dir)),chisq_mean)
-#np.savetxt(os.path.abspath("injections/{0}/{1}/chisq_stdev.txt".format(params.label, data_dir)),chisq_stdev)
 np.savetxt(os.path.abspath(os.path.join(data_dir, "chisq_mean_" + ifo + ".txt")),chisq_mean)
 np.savetxt(os.path.abspath(os.path.join(data_dir, "chisq_stdev_" + ifo + ".txt")),chisq_stdev)
 
 
 #################################################################
-#table = Table(SNR_array)
-#table.write(os.path.abspath("injections/{}/Inj_SNR.csv".format(data_dir)), format='csv')
 GWData = dict()
 
-#strain_raw = process_data.load_data(params.IFO)
 strain_raw = process_data.load_data(ifo)
 print("Data loaded")
-#strain_clean = process_data.clean(strain_raw, spec_lines = process_data.spec_lines['{}_lines'.format(params.IFO), 30., 600.)
 #print("Data cleaned")
-
-#GWData['{}_strain_raw'.format(params.IFO)] = strain_raw
-#GWData['{}_strain_clean'.format(params.IFO)] = strain_clean
 
 strain_nnetfix, start, end = process_data.crop_for_nnetfix(strain_raw)
 print("Data cropped for NNetfixing")
@@ -209,24 +176,10 @@ snrlist.append(snrp)
 snr, snrp, snrl = metrics.calculate_snr(strain_original, 35, 29)
 print("The new original SNR is {} at {}.".format(snrp, snrl))
 
-#np.savetxt(os.path.abspath("injections/{0}/{1}/real_data.txt".format(params.label, data_dir)),np.array(snrlist))
 np.savetxt(os.path.abspath(os.path.join(data_dir, "real_data_" + ifo + ".txt")),np.array(snrlist))
 
-
-#strain_reconstructed.name = "{}:GDS-CALIB_STRAIN".format(params.IFO)
-#strain_gated.name = "{}:GDS-CALIB_STRAIN".format(params.IFO)
 strain_reconstructed.name = "{}:GDS-CALIB_STRAIN".format(ifo)
 strain_gated.name = "{}:GDS-CALIB_STRAIN".format(ifo)
-
-
-np.savetxt(os.path.abspath(os.path.join(data_dir, "original_{0}_{1}.txt".format(i, ifo))),OriginalData)
-#strain_reconstructed.write(os.path.abspath("injections/{0}/{1}/{2}_{3}_reconstructed.gwf".format(params.label, data_dir,params.IFO,params.label)))
-#strain_gated.write(os.path.abspath("injections/{0}/{1}/{2}_{3}_gated.gwf".format(params.label, data_dir,params.IFO,params.label)))
-#strain_original.write(os.path.abspath("injections/{0}/{1}/{2}_{3}_original.gwf".format(params.label, data_dir,params.IFO,params.label)))
-
-#strain_reconstructed.write(os.path.abspath(os.path.join(data_dir, "{0}_{1}_reconstructed.gwf".format(IFO,params.label))))
-#strain_gated.write(os.path.abspath(os.path.join(data_dir, "{0}_{1}_gated.gwf".format(IFO,params.label))))
-#strain_original.write(os.path.abspath(os.path.join(data_dir, "{0}_{1}_original.gwf".format(IFO,params.label))))
 
 strain_reconstructed.write(os.path.abspath(os.path.join(data_dir, "{0}_{1}_reconstructed.gwf".format(ifo,params.label))))
 strain_gated.write(os.path.abspath(os.path.join(data_dir, "{0}_{1}_gated.gwf".format(ifo,params.label))))
